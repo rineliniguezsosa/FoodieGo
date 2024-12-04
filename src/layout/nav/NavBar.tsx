7de8941ceaf853d64a6800a834441ca494b99dfc
Rinel <rineliniguezsosa@gmail.com>
2024-12-03 18:38:26 -0600

    [FEAT] Estableciendo la prop className para darle estilos a los elementos header y nav para observar el comportamiento, estableciendo el elemento h1 con el titulo de la app Foodie Go
    - css en el header con un width full, tamaño y color de borde
    - css en el nav con un width full y tamaño y color de borde

--- src/layout/nav/NavBar.tsx
+++ src/layout/nav/NavBar.tsx
@@ -2,9 +2,12 @@
 
 export const NavBar = () => {
   return (
-    <header>
-        <nav>
-            
+    <header className="w-full border-2 border-red-400">
+        <nav className="w-full border-2 border-blue-400">
+          <div>
+            <h1>Foodie Go</h1>
+          </div>
+        
         </nav>
     </header>
   )
