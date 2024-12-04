275141df44b1620c8df0770fdc396d573031ce36
Rinel <rineliniguezsosa@gmail.com>
2024-12-03 19:52:56 -0600

    [FEAT] Cambios en el archivo NavBar.tsx
    - cambios en el elemento html nav, establecemos padding izquierdo y derecho de 1rem, establecemos padding en el eje y de 0.5rem, justificamos los elementos a lo ancho del elemento nav

--- src/layout/nav/NavBar.tsx
+++ src/layout/nav/NavBar.tsx
@@ -4,7 +4,7 @@ import { ShoopingBadge } from '../../components';
 export const NavBar = () => {
   return (
     <header className="w-full border-2 border-red-400">
-        <nav className="w-full flex border-2 border-blue-400">
+        <nav className="w-full px-4 py-2 justify-between flex border-2 border-blue-400">
           <div>
             <h1>Foodie Go</h1>
           </div>
