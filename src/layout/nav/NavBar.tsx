838aa4e1c84f64b722fc206cd0b825fa33bb955e
Rinel <rineliniguezsosa@gmail.com>
2024-12-03 19:42:58 -0600

    [FEAT] Cambios en el archivo NavBar.tsx
    - establecemos la prop flex para usar display flex en el elemento html nav
    - importamos el componente ShoopingBadge
    - establecemos el elemento html div con el componente ShoopingBadge

--- src/layout/nav/NavBar.tsx
+++ src/layout/nav/NavBar.tsx
@@ -1,15 +1,20 @@
 import LocationIcon from '../../assets/basic-icons/localization-icon.svg';
+import { ShoopingBadge } from '../../components';
 
 export const NavBar = () => {
   return (
     <header className="w-full border-2 border-red-400">
-        <nav className="w-full border-2 border-blue-400">
+        <nav className="w-full flex border-2 border-blue-400">
           <div>
             <h1>Foodie Go</h1>
           </div>
 
           <div>
             <img className='text-green' src={LocationIcon} alt="location-icon" />
+          </div>
+
+          <div>
+            <ShoopingBadge/>
           </div>
         
         </nav>
