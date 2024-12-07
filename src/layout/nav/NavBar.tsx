58a6d077593671734393b3fef1039b912389c46b
Rinel <rineliniguezsosa@gmail.com>
2024-12-06 18:31:25 -0600

    [FEAT] Importando el hook useEffect de react
    - definiendo la función useEffect sin dependencias
    - este hook servira para obtener la ubicación del usuario mediante geolocation api

--- src/layout/nav/NavBar.tsx
+++ src/layout/nav/NavBar.tsx
@@ -2,10 +2,15 @@ import { IconButton,Drawer } from '@mui/material';
 import LocationIcon from '../../assets/basic-icons/localization-icon.svg';
 import { ShoopingBadge } from '../../components';
 import MenuIcon from '@mui/icons-material/Menu';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 export const NavBar = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  useEffect(() => {
+    
+  }, [])
+  
   return (
     <header className="w-full border-2 border-red-400 bg-bluegray">
         <nav className="w-full px-4 py-2 justify-between items-center flex border-2 border-blue-400">
