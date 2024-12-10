feae9e27cd8894e0b0ccde132b7ba895790ae18e
Rinel <rineliniguezsosa@gmail.com>
2024-12-09 20:26:29 -0600

    [FEAT] Pegando mi tipado de mi respuesta de la api de openstreetmap al pasar los datos de latitud y longitud
    - creando la interface Place y Address

---
+++ src/types/openstreetmap.types.ts
@@ -0,0 +1,29 @@
+export interface Place {
+    place_id:     number;
+    licence:      string;
+    osm_type:     string;
+    osm_id:       number;
+    lat:          string;
+    lon:          string;
+    class:        string;
+    type:         string;
+    place_rank:   number;
+    importance:   number;
+    addresstype:  string;
+    name:         string;
+    display_name: string;
+    address:      Address;
+    boundingbox:  string[];
+}
+
+export interface Address {
+    road:             string;
+    residential:      string;
+    city:             string;
+    county:           string;
+    state:            string;
+    "ISO3166-2-lvl4": string;
+    postcode:         string;
+    country:          string;
+    country_code:     string;
+}
